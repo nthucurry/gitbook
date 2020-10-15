f1c7805308277153456e408acb2c60fc919b806f
fool <fool@iMac11.local>
2020-10-15 23:17:33 +0800

    update

--- knowledge/oracle/script/os-utility.sh
+++ knowledge/oracle/script/os-utility.sh
@@ -1,25 +1,45 @@
 #/bin/bash
 
-month=`date +%Y-%m`
-output_file="${month}-cpu-utility.csv"
+. ~/.bash_profile
 
-sqlplus -s system/oracle << EOF
+month=`date +%Y-%m`
+main_path=/home/oracle/get_os_usage
+
+cat $main_path/login-info.csv | while read line
+do
+
+if [[ $line == *"$"* ]]; then
+echo $line
+else
+
+account=`echo $line | awk 'BEGIN {FS=","} {print $1}'`
+password=`echo $line | awk 'BEGIN {FS=","} {print $2}'`
+sid=`echo $line | awk 'BEGIN {FS=","} {print $3}'`
+output_file=${main_path}/${month}-${sid}-cpu-utility.csv
+
+$ORACLE_HOME/bin/sqlplus -s ${account}/${password}@${sid} << EOF
 
 set term off
 set heading off
-set pagesize 0
+set pagesize 24
 set linesize 200
 set trimout on
 set trimspool on
 set echo off
 set termout off
+set feedback off
 
-spool temp.log
-@os-utility.sql
+spool /home/oracle/get_os_usage/temp.log
+@/home/oracle/get_os_usage/os-cpu.sql
 spool off
 quit
 /
 EOF
 
-cat temp.log >> $output_file
+cat $main_path/temp.log >> $output_file
 sed -i "/^$/d" $output_file
+rm $main_path/temp.log
+
+fi
+
+done
--- knowledge/oracle/script/os-utility.sql
+++ knowledge/oracle/script/os-utility.sql
@@ -1,3 +1,4 @@
+-- CPU
 SELECT
     (SELECT name FROM v$database)||','||
     instance_number||','||
@@ -9,9 +10,31 @@ SELECT
     maxval||','||
     average||','||
     standard_deviation||','||
-    sum_squares
+    sum_squares AS CPU
 FROM DBA_HIST_SYSMETRIC_SUMMARY
 WHERE
     metric_name = 'Host CPU Utilization (%)'
-    AND begin_time BETWEEN sysdate - 1 AND sysdate - 0
-ORDER BY begin_time DESC;
+    AND end_time BETWEEN to_date(to_char(sysdate-1,'YYYY-MM-DD'),'YYYY-MM-DD HH24:MI:SS')
+                     AND to_date(to_char(sysdate-1,'YYYY-MM-DD')||'23:59:59','YYYY-MM-DD HH24:MI:SS')
+ORDER BY end_time;
+
+-- RAM
+SELECT * FROM v$sysmetric_summary WHERE metric_name LIKE '%PGA%';
+SELECT
+    (SELECT name FROM v$database)||','||
+    instance_number||','||
+    to_char(begin_time,'YYYY-MM-DD HH24:MI:SS')||','||
+    to_char(end_time,'YYYY-MM-DD HH24:MI:SS')||','||
+    metric_name||','||
+    /*metric_unit,*/
+    minval/1024/1024||','||
+    maxval/1024/1024||','||
+    average/1024/1024||','||
+    standard_deviation/1024/1024||','||
+    sum_squares/1024/1024 AS RAM
+FROM DBA_HIST_SYSMETRIC_SUMMARY
+WHERE
+    metric_name = 'Total PGA Allocated'
+    AND end_time BETWEEN to_date(to_char(sysdate-1,'YYYY-MM-DD'),'YYYY-MM-DD HH24:MI:SS')
+                     AND to_date(to_char(sysdate-1,'YYYY-MM-DD')||'23:59:59','YYYY-MM-DD HH24:MI:SS')
+ORDER BY end_time;
