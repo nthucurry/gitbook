1ee99f207fa0d88864cc7151a159ea0c8821cdfd
fool <fool@iMac11.local>
2020-11-14 18:39:27 +0800

    update

--- knowledge/oracle/script/os-init-setting.sh
+++ knowledge/oracle/script/os-init-setting.sh
@@ -1,6 +1,19 @@
 #/bin/bash
 
-proxy_server='proxy'
+### define
+proxy_server=occ
+proxy_ip="10.140.0.10"
+user=oracle
+sid=RAC
+uqname=$sid
+base=/u01/oracle
+ora_ver=12201
+home=$base/$ora_ver
+bpfile=.bash_profile_ora
+swap_size=8
+
+### DNS
+echo "$proxy_ip $proxy_server" >> /etc/hosts
 
 ### internet connection
 echo "proxy=http://$proxy_server:80" >> /etc/yum.conf
@@ -26,7 +39,7 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 source ~/.bashrc
 
 ### swap
-dd if=/dev/zero of=/swapfile count=8 bs=1GiB
+dd if=/dev/zero of=/swapfile count=$swap_size bs=1GiB
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
@@ -40,16 +53,61 @@ systemctl disable firewalld
 ### oracle prerequisite
 wget http://public-yum.oracle.com/public-yum-ol7.repo -O /etc/yum.repos.d/public-yum-ol7.repo
 wget http://public-yum.oracle.com/RPM-GPG-KEY-oracle-ol7 -O /etc/pki/rpm-gpg/RPM-GPG-KEY-oracle
-yum install oracle-rdbms-server-11gR2-preinstall -y
+if [ "$ora_ver" -eq 11204 ]; then
+    yum install oracle-rdbms-server-11gR2-preinstall -y
+else
+    yum install oracle-database-server-12cR2-preinstall -y
+fi
 
 ### group
-# groupadd -g 501 dba
-# groupadd -g 502 oinstall
+groupadd -g 54321 oinstall
+groupadd -g 54322 dba
+groupadd -g 54323 oper
+groupadd -g 54324 backupdba
+groupadd -g 54325 dgdba
+groupadd -g 54326 kmdba
+groupadd -g 54327 asmdba
+groupadd -g 54328 asmoper
+groupadd -g 54329 asmadmin
+groupadd -g 54330 racdba
 
 ### directory
 mkdir /u01
 mkdir /oracle
 mkdir /backup
 
+### owner
+chown -R $user:dba /u01
+
 ### enviromnent variable
 LANG=en_US.UTF-8
+
+### oracle account setting
+cat > /home/$user/$bpfile <<EOF
+export ORACLE_SID=$sid
+export ORACLE_UNQNAME=$uqname # it is difference between primary and standby database
+export ORACLE_BASE=$base
+export ORACLE_HOME=$home
+export TNS_ADMIN=$home/network/admin
+LD_LIBRARY_PATH=$home/lib:/lib:/usr/lib;
+export LD_LIBRARY_PATH
+CLASSPATH=$home/JRE:$home/jlib:$home/rdbms/jlib;
+export CLASSPATH
+PATH=$PATH:$home/bin:$home/bin
+export PATH
+
+# Alias
+alias sqlp='sqlplus / as sysdba'
+alias rm='rm -i'
+alias vi='vim'
+alias grep='grep --color=always'
+alias tree='tree --charset ASCII'
+alias bdump="cd $base/diag/rdbms/${uqname,,}/$sid/trace"
+EOF
+
+chown $user:$user /home/$user/$bpfile
+
+echo ". /home/$user/$bpfile" >> /home/$user/.bash_profile
+
+su - $user
+source ~/.bash_profile
