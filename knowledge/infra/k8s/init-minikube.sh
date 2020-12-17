6b154cf499ca79fe05ce4f4318a2cf2ee2ae92b6
fool <fool@iMac11.local>
2020-12-17 22:31:23 +0800

    minikube

---
+++ knowledge/infra/k8s/init-minikube.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# https://github.com/kubernetes/minikube/
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin
+
+yum install yum-utils device-mapper-persistent-data lvm2 -y
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce -y
+systemctl start docker && systemctl enable docker
+usermod -aG docker USERNAME
