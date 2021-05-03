b9ed9485ca3326d4b516a28231870df34e2cd0f4
docker <docker@MBP18.local>
2021-05-03 22:36:28 +0800

    update

--- knowledge/cloud/azure/script/delete-vm.sh
+++ knowledge/cloud/azure/script/delete-vm.sh
@@ -2,17 +2,19 @@
 ###################################
 subscription="de61f224-9a69-4ede-8273-5bcef854dc20"
 resource_group="DBA-K8S"
-vm_name="t-k8s-lb"
-nic="vm-k8s-m2588"
+vm_name=$1
 ###################################
 az account set -s $subscription
 ###################################
+
 if [[ $resource_group == "DBA-K8S" ]]; then
 
     disk_name=`az vm list -g $resource_group -d --query "[?name == '$vm_name'].storageProfile.osDisk.name" -o tsv`
+    nic=`az vm list -g $resource_group -d --query "[?name == '$vm_name'].networkProfile.networkInterfaces"`
+
     az vm delete -g $resource_group -n $vm_name --yes
-    # az network nic delete -g $resource_group -n $nic
     az disk delete -g $resource_group -n $disk_name --yes
+    # az network nic delete -g $resource_group -n $nic
 
 else
     echo "[Warning...] It is not test resource group!!"
