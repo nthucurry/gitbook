28f0218a21629d06a8a8b9be0731cfc4aa7ad894
MBP18 <docker@MBP18.local>
2021-06-30 22:31:23 +0800

    vm

--- knowledge/cloud/azure/script/create-vm.sh
+++ knowledge/cloud/azure/script/create-vm.sh
@@ -5,8 +5,8 @@
 subscription="a7bdf2e3-b855-4dda-ac93-047ff722cbbd"
 resource_group="DBA_Test"
 ###################################
-vm_name="t-m2"
-image="OpenLogic:CentOS-LVM:7-lvm-gen2:7.9.2021020401"
+vm_name=$1
+image="OpenLogic:CentOS:7_9:7.9.2021020400"
 size="Standard_B2s" # CPU, RAM
 os_disk_size="30" # GB
 admin="azadmin"
@@ -14,6 +14,7 @@ password="AzureK8S2021"
 location="southeastasia"
 subnet="Infra"
 vnet="BigDataVNet"
+ssh_key_values="~/.ssh/id_rsa.pub"
 ###################################
 az account set -s $subscription
 echo "[Subscription.....] "`az account show --query name`
@@ -23,7 +24,7 @@ echo "[Resource Group...] "$resource_group
 az vm create \
 --name $vm_name \
 --resource-group $resource_group \
---admin-password $password \
+#--admin-password $password \
 --admin-username $admin \
 --enable-agent true \
 --image $image \
@@ -36,4 +37,11 @@ az vm create \
 --public-ip-address "" \
 --nics $vm_name \
 --license-type none \
---authentication-type password
+--authentication-type ssh \
+--ssh-key-values $ssh_key_values
+
+###################################
+
+ssh $admin@$vn_name sudo timedatectl set-timezone Asia/Taipei
+ssh $admin@$vn_name wget https://raw.githubusercontent.com/ShaqtinAFool/gitbook/master/knowledge/infra/k8s/initial-k8s.sh
+ssh $admin@$vn_name chmod +x initial-k8s.sh
