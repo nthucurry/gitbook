a79cc399234cbeb6034c50f426094b9c43b1c8ed
MBP18 <docker@MBP18.local>
2021-07-10 14:16:08 +0800

    wkc

--- knowledge/infra/openshift/script/1-initial-setting.sh
+++ knowledge/infra/openshift/script/1-initial-setting.sh
@@ -1,8 +1,8 @@
 sudo timedatectl set-timezone Asia/Taipei
-sudo yum update -y
-sudo yum install epel-release -y
-sudo yum install htop telnet nc nmap -y
-sudo yum install nfs-utils -y
+sudo yum update -y | grep "Complete!"
+sudo yum install epel-release -y | grep "Complete!"
+sudo yum install htop telnet nc nmap -y | grep "Complete!"
+sudo yum install nfs-utils -y | grep "Complete!"
 
 # azure cli
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -12,7 +12,7 @@ baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
-sudo yum install azure-cli -y
+sudo yum install azure-cli -y | grep "Complete!"
 
 # openshift install package
 cd ~
@@ -25,6 +25,13 @@ tar xvf openshift-install-linux-4.5.36.tar.gz
 cd ~
 mkdir ocp4.5_cust
 wget https://raw.githubusercontent.com/ShaqtinAFool/gitbook/master/knowledge/infra/openshift/install-config.yaml
+
+# generate ssh key
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
+ssh_key_values="`cat ~/.ssh/id_rsa.pub`"
+
+# update ssh key in install-config.yaml
+sed -i "s|ssh-rsa XXXX|$ssh_key_values|g" ~/install-config.yaml
 cp ./install-config.yaml ./ocp4.5_cust
 
 # openshift client tool
