cd4aef6e2dda6ade8161cc151f9b9388f3e613e6
MBP18 <docker@MBP18.local>
2021-07-21 23:07:21 +0800

    azure

---
+++ knowledge/cloud/azure/script/subnet-detail.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+subscription="a7bdf2e3-b855-4dda-ac93-047ff722cbbd"
+resource_group="Global"
+vnet="BigDataVNet"
+
+az network vnet subnet list \
+--resource-group $resource_group \
+--vnet-name $vnet \
+--query "[].name" -o tsv | tee -a subnet-list.txt
+
+cat subnet-list.txt | while read subnet
+do
+    # echo "az network vnet subnet show -g $resource_group -n $subnet --vnet-name $vnet"
+    az network vnet subnet show -g $resource_group -n $subnet --vnet-name $vnet \
+    --query [].{subnet: name, ip: addressPrefix, nsg: networkSecurityGroup.name, route:routeTable.name} \
+    -o json
+done
+
+rm subnet-list.txt
