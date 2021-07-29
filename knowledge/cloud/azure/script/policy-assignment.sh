d2fcab98aca3a5a706b1a99d99ec287c1e8973b5
MBP18 <docker@MBP18.local>
2021-07-29 22:32:22 +0800

    azure

--- knowledge/cloud/azure/script/policy-assignment.sh
+++ knowledge/cloud/azure/script/policy-assignment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ -e file ] && rm policy_assignment_list_name.tmp
+[ -e file ] && rm policy_assignment_list.json
+
 # policy assignment list
 az policy assignment list --query "[].name" -o tsv > policy_assignment_list_name.tmp
 
@@ -9,13 +12,13 @@ do
     policy_assignment_id=$name
     az policy assignment show \
     --name $policy_assignment_id \
-    --query "{ \
+    --query "[].{ \
     display_name:displayName, \
     not_scopes:notScopes, \
     parameter_effect_value:parameters.effect.value, \
     policy_definition_id:policyDefinitionId, \
     scope:scope \
-    }" -o table
+    }" >> policy_assignment_list.json
 done
 
 rm policy_assignment_list_name.tmp
