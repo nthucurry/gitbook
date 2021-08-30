cade076228de09a6d894334955fc5732bb32b774
MBP18 <docker@MBP18.local>
2021-08-30 22:15:49 +0800

    ocp

--- knowledge/infra/openshift/script/backup-wkc.sh
+++ knowledge/infra/openshift/script/backup-wkc.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+service_type="dev"
 backup_time=`date +%Y-%m%d`
 
 /home/azadmin/ibm/cpd-cli backup-restore quiesce -n zen \
-    > /home/azadmin/cmd/log/wkc-backup-quiesce-$backup_time.log
+    > /mnt/backup/wkc/$service_type/wkc-backup-quiesce-$backup_time.log
 /home/azadmin/ibm/cpd-cli backup-restore volume-backup create -n zen cpdbk-$backup_time --skip-quiesce=true \
-    > /home/azadmin/cmd/log/wkc-backup-volume-$backup_time.log
+    > /mnt/backup/wkc/$service_type/wkc-backup-volume-$backup_time.log
 /home/azadmin/ibm/cpd-cli backup-restore unquiesce -n zen \
-    > /home/azadmin/cmd/log/wkc-backup-unquiesce-$backup_time.log
+    > /mnt/backup/wkc/$service_type/wkc-backup-unquiesce-$backup_time.log
 
 delete_time=`date +%Y-%m%d --date="-7 day"`
 /home/azadmin/ibm/cpd-cli backup-restore volume-backup purge cpdbk-$delete_time
--- knowledge/infra/openshift/script/mount-aznfs.sh
+++ knowledge/infra/openshift/script/mount-aznfs.sh
@@ -1,3 +1,4 @@
 mount -o sec=sys,vers=3,nolock,proto=tcp wkcnfs.blob.core.windows.net:/wkcnfs/etcd /mnt/backup/etcd
 mount -o sec=sys,vers=3,nolock,proto=tcp wkcnfs.blob.core.windows.net:/wkcnfs/config /mnt/backup/config
 mount -o sec=sys,vers=3,nolock,proto=tcp wkcnfs.blob.core.windows.net:/wkcnfs/nfs-disk-space /mnt/backup/nfs-disk-space
+mount -o sec=sys,vers=3,nolock,proto=tcp wkcnfs.blob.core.windows.net:/wkcnfs/wkc /mnt/backup/wkc
