9780d8f082784cae105a2942efc93134daadfdb4
MBP18 <docker@MBP18.local>
2021-09-06 21:02:33 +0800

    wkc

--- knowledge/infra/openshift/script/backup-wkc.sh
+++ knowledge/infra/openshift/script/backup-wkc.sh
@@ -5,10 +5,15 @@ backup_time=`date +%Y-%m%d`
 
 /home/azadmin/ibm/cpd-cli backup-restore quiesce -n zen \
     > /mnt/backup/wkc/$service_type/wkc-backup-quiesce-$backup_time.log
+chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-quiesce-$backup_time.log
+
 /home/azadmin/ibm/cpd-cli backup-restore volume-backup create -n zen cpdbk-$backup_time --skip-quiesce=true \
     > /mnt/backup/wkc/$service_type/wkc-backup-volume-$backup_time.log
+chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-volume-$backup_time.log
+
 /home/azadmin/ibm/cpd-cli backup-restore unquiesce -n zen \
     > /mnt/backup/wkc/$service_type/wkc-backup-unquiesce-$backup_time.log
+chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-unquiesce-$backup_time.log
 
 delete_time=`date +%Y-%m%d --date="-7 day"`
 /home/azadmin/ibm/purge-wkc-bk $delete_time
--- knowledge/infra/openshift/script/check-nfs-disk.sh
+++ knowledge/infra/openshift/script/check-nfs-disk.sh
@@ -4,3 +4,4 @@ service_type="dev"
 check_time=`date +%Y-%m%d`
 
 ssh dev-nfs 'df -h' > /mnt/backup/nfs-disk-space/$service_type/disk-space-$check_time.log
+chmod 755 /mnt/backup/nfs-disk-space/$service_type/disk-space-$check_time.log
