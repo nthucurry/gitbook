b76eace418e7384d4ac87dee11e1853172fc4203
MBP18 <docker@MBP18.local>
2021-09-08 21:09:36 +0800

    wkc

--- knowledge/infra/openshift/script/backup-wkc.sh
+++ knowledge/infra/openshift/script/backup-wkc.sh
@@ -5,15 +5,15 @@ backup_time=`date +%Y-%m%d`
 
 /home/azadmin/ibm/cpd-cli backup-restore quiesce -n zen \
     > /mnt/backup/wkc/$service_type/wkc-backup-quiesce-$backup_time.log
-chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-quiesce-$backup_time.log
+# chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-quiesce-$backup_time.log
 
 /home/azadmin/ibm/cpd-cli backup-restore volume-backup create -n zen cpdbk-$backup_time --skip-quiesce=true \
     > /mnt/backup/wkc/$service_type/wkc-backup-volume-$backup_time.log
-chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-volume-$backup_time.log
+# chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-volume-$backup_time.log
 
 /home/azadmin/ibm/cpd-cli backup-restore unquiesce -n zen \
     > /mnt/backup/wkc/$service_type/wkc-backup-unquiesce-$backup_time.log
-chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-unquiesce-$backup_time.log
+# chmod 755 /mnt/backup/wkc/$service_type/wkc-backup-unquiesce-$backup_time.log
 
 delete_time=`date +%Y-%m%d --date="-7 day"`
 /home/azadmin/cmd/purge-wkc-bk.exp $delete_time
