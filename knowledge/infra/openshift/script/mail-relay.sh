e98db9313cf5a44cd781ad226a9e5779dbf0721d
iMac11 <fool@iMac11.local>
2021-09-12 17:50:10 +0800

    update

--- knowledge/infra/openshift/script/mail-relay.sh
+++ knowledge/infra/openshift/script/mail-relay.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+/usr/bin/expect << EOF
+spawn telnet mail-relay.example.com 25
+send "EHLO auo.com"
+send "MAIL FROM: <alert@example.com>"
+send "RCPT TO: <user@example.com>"
+send "DATA"
+send "Subject: test"
+send "Hello,"
+send "This is an email sent by using the telnet command."
+send "."
+send "quit"
+interact
+eof
