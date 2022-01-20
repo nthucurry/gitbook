ec608e1e3c912cbb8e395bd234c6fc88ee92232c
MBP18 <docker@MBP18.local>
2022-01-20 19:48:36 +0800

    update

--- elk/script/sync-blob-by-blobfuse.sh
+++ elk/script/sync-blob-by-blobfuse.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 source $HOME/.bash_profile
 
-getYY=`date +%Y --date="-9 Hour"`
-getMM=`date +%m --date="-9 Hour"`
-getDD=`date +%d --date="-9 Hour"`
-getHH=`date +%H --date="-9 Hour"` # LST -> UTC
+HH=$1
+[ ! -z $1 ] && HH=$1 || HH=9
+
+getYY=`date +%Y --date="-$HH Hour"`
+getMM=`date +%m --date="-$HH Hour"`
+getDD=`date +%d --date="-$HH Hour"`
+getHH=`date +%H --date="-$HH Hour"` # LST -> UTC
 delYY=`date +%Y --date="-22 Hour"`
 delMM=`date +%m --date="-22 Hour"`
 delDD=`date +%d --date="-22 Hour"`
 delHH=`date +%H --date="-22 Hour"` # LST -> UTC
 blobList="$HOME/sync-blob-list.txt"
 
-# echo "______ $getYY/$getMM/$getDD ______" >> $HOME/_sync-blob_$getYY-$getMM$getDD.log
+# echo "______ $getYY/$getMM/$getDD ______"
 
 cat $blobList | while read srcPath
 do
@@ -20,9 +23,9 @@ do
   fi
   dstPath=`echo $srcPath | sed 's/data-from-cloud/data-to-local/g'`
   mkdir -p $dstPath/y=$getYY/m=$getMM/d=$getDD
-  # echo "  mkdir -p $dstPath/y=$getYY/m=$getMM/d=$getDD"
+  # echo "  1.  mkdir -p $dstPath/y=$getYY/m=$getMM/d=$getDD"
   cp -fr $srcPath/y=$getYY/m=$getMM/d=$getDD/h=$getHH $dstPath/y=$getYY/m=$getMM/d=$getDD
-  # echo "    cp -fr $srcPath/y=$getYY/m=$getMM/d=$getDD/h=$getHH $dstPath/y=$getYY/m=$getMM/d=$getDD"
+  # echo "  2.  cp -fr $srcPath/y=$getYY/m=$getMM/d=$getDD/h=$getHH $dstPath/y=$getYY/m=$getMM/d=$getDD"
   if [[ $srcPath == *"NETWORKSECURITYGROUP"* ]];then
     # echo "      ~/update-file-for-nsg.sh $dstPath/y=$getYY/m=$getMM/d=$getDD/h=$getHH"
     $HOME/update-file-for-nsg.sh $dstPath/y=$getYY/m=$getMM/d=$getDD/h=$getHH
