b479a645fee3abcb485afa8305b6dbac25e9b8f2
iMac11 <fool@iMac11.local>
2022-03-28 10:47:02 +0800

    update

--- cloud/azure/script/create-firewall.sh
+++ cloud/azure/script/create-firewall.sh
@@ -2,29 +2,32 @@ subscription="de61f224-9a69-4ede-8273-5bcef854dc20"
 resourceGroup="DBA-K8S"
 location="southeastasia"
 firewall="fortiweb-fw"
-firewallIPConfig="fortiweb-fw"
+policy="fortiweb-fw"
+IPConfig="fortiweb-fw"
 publicIP="fortiweb-fw"
 vnet="t-vnet"
 
-az network firewall create \
-    --name $firewall \
-    --resource-group $resourceGroup \
-    --location $location
+az configure --defaults group=$resourceGroup
 
-az network public-ip create \
+# az network firewall create \
+#     --name $firewall \
+#     --resource-group $resourceGroup \
+#     --location $location
+
+# az network public-ip create \
+#     --name $publicIP \
+#     --resource-group $resourceGroup \
+#     --location $location \
+#     --allocation-method static \
+#     --sku standard
+
+az network firewall ip-config create \
+    --firewall-name $firewall \
     --name $publicIP \
-    --resource-group $resourceGroup \
-    --location $location \
-    --allocation-method static \
-    --sku standard
+    --public-ip-address $publicIP \
+    --vnet-name $vnet
 
-# az network firewall ip-config create \
-#     --firewall-name $firewall \
-#     --name FW-config \
-#     --public-ip-address fw-pip \
-#     --resource-group Test-FW-RG \
-#     --vnet-name Test-FW-VN
-
-az network firewall update \
-    --name $firewall \
-    --resource-group $resourceGroup
+# az network firewall update \
+#     --name $firewall \
+#     --resource-group $resourceGroup \
+#     --firewall-policy $policy
