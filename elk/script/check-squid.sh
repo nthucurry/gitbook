d3bff7dedb13d18833553a9d6d90c09e3803eed9
iMac11 <Tony>
2022-04-30 13:37:24 +0800

    update

--- elk/script/check-squid.sh
+++ elk/script/check-squid.sh
@@ -1,1 +1,6 @@
-cat /data-to-local/*/azure-squid/proxy-*/access.log | grep $1 | awk '{print $7}' | sort > tmp.txt; uniq tmp.txt -c
+#!/bin/bash
+targetIP=$1
+cat /data-from-cloud/eda-azure-squid/proxy-*/access.log | grep $targetIP | awk '{print $3,$7}' | sort > tmp.txt
+uniq tmp.txt -c > tmp1.txt
+sort -rn tmp1.txt
+rm tmp*.txt
