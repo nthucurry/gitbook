40db2114c2310e2000751675726a8288615b53ee
MBP18 <xu3ej04u454@gmail.com>
2022-05-05 22:26:22 +0800

    squid

--- elk/script/check-squid.sh
+++ elk/script/check-squid.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-targetIP=$1
-cat /data-from-cloud/eda-azure-squid/proxy-*/access.log | grep $targetIP | awk '{print $3,$7}' | sort > tmp.txt
-uniq tmp.txt -c > tmp1.txt
-sort -rn tmp1.txt
+#1650765033.170 488858 10.248.12.50 TCP_TUNNEL/200 14165 CONNECT management.azure.com:443 - HIER_DIRECT/40.78.234.177 -
+
+targetIPs=$1
+if [[ -z $targetIPs ]];then
+  cat /data-from-cloud/eda-azure-squid/proxy-*/access.log | awk '{print $7}' | sort > tmp1.txt
+else
+  cat /data-from-cloud/eda-azure-squid/proxy-*/access.log | grep -E $targetIPs | awk '{print $7}' | sort > tmp1.txt
+fi
+uniq tmp1.txt -c > tmp2.txt
+sort -rn tmp2.txt
 rm tmp*.txt
